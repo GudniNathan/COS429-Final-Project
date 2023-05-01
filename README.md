dfb6dcb32329746801c37f06cb34e9cc5c2dd0f7
Guðni Natan Gunnarsson <1493259+GudniNathan@users.noreply.github.com>
2023-05-01 18:12:28 -0400

    Start

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # COS429-Final-Project
  
+To install requirements, please run:
+```conda env create -f environment.yml```
+
+Then activate the new environment:
+```conda activate COS429-Final-Project```
+
+If you need to update the environment, run:
+```conda env update --file environment.yml --prune```
---
+++ rasterize.py
@@ -0,0 +1,82 @@
+"""
+This module contains functions that create images of a given 3d object 
+from a given camera perspective.
+"""
+
+import numpy as np
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from objloader import *
+import sys
+
+
+pygame.init()
+viewport = (800, 600)
+hx = viewport[0] / 2
+hy = viewport[1] / 2
+srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
+obj = OBJ('objects/rubberduckie/rubberduckie.obj')
+
+glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
+glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
+glEnable(GL_LIGHT0)
+glEnable(GL_LIGHTING)
+glEnable(GL_COLOR_MATERIAL)
+glEnable(GL_DEPTH_TEST)
+glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
+
+# LOAD TEXTURES
+texture = glGenTextures(1)
+glBindTexture(GL_TEXTURE_2D, texture)
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+# texture = loadTexture('objects/rubberduckie/rubberduckie.png')
+
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+width, height = viewport
+gluPerspective(90.0, width/float(height), 1, 100.0)
+glEnable(GL_DEPTH_TEST)
+glMatrixMode(GL_MODELVIEW)
+
+rx, ry = (0,0)
+tx, ty = (0,0)
+zpos = 5
+rotate = move = False
+while 1:
+    for e in pygame.event.get():
+        if e.type == QUIT:
+            sys.exit()
+        elif e.type == KEYDOWN and e.key == K_ESCAPE:
+            sys.exit()
+        elif e.type == MOUSEBUTTONDOWN:
+            if e.button == 4: zpos = max(1, zpos-1)
+            elif e.button == 5: zpos += 1
+            elif e.button == 1: rotate = True
+            elif e.button == 3: move = True
+        elif e.type == MOUSEBUTTONUP:
+            if e.button == 1: rotate = False
+            elif e.button == 3: move = False
+        elif e.type == MOUSEMOTION:
+            i, j = e.rel
+            if rotate:
+                rx += i
+                ry += j
+            if move:
+                tx += i
+                ty -= j
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+
+    # RENDER OBJECT
+    glTranslate(tx/20., ty/20., - zpos)
+    glRotate(ry, 1, 0, 0)
+    glRotate(rx, 0, 1, 0)
+    glCallList(obj.gl_list)
+
+    pygame.display.flip()
+    pygame.time.wait(10)
