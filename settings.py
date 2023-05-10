e6f7535a3b44afceeb48df5efc0e02277e2fd926
fairaque1999 <53129458+fairaque1999@users.noreply.github.com>
2023-05-09 20:18:21 -0700

    settings edit

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 
 # This file contains all the settings.
 # Update this file to change the settings of the program.
+VIDEO_NAME = "desk_1"
 VIDEO_FILE_PATH = "videos/desk_1.mp4"
 OUTPUT_FILE_PATH = "output/desk_1.mp4"
 GROUNDTRUTH_FILE_PATH = "videos/desk_1_groundtruth_interpolated.txt"
@@ -22,8 +23,9 @@ SKIP_START = 5
 
 # The position of the 3d object, relative to the camera at frame 0.
 # Note that the camera is at (0, 0, 0) and is looking at (0, 0, -1).
-OBJECT_POSITION = (-20., -10., -20.)
+# OBJECT_POSITION = (-20., -10., -20.)
 # OBJECT_POSITION = (20., 20., 20.)
+OBJECT_POSITION = (0., 0, -30.) # For video xyz
 
 # The rotation of the 3d object, relative to the camera at frame 0.
 OBJECT_ROTATION = [0., 0., 0.]
